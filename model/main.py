78c4af96573e99c487d122029d4c0d77d1078d8f
Darryl Lardizabal <dsteven12@gmail.com>
2024-02-17 00:06:25 -0800

    Created clean data function that removed cols: Unnamed: 32, id; and remapped diagnosis column from M & B to 1 and 0 respectively.

--- model/main.py
+++ model/main.py
@@ -1,5 +1,14 @@
-def main():
+import pandas as pd
 
+def get_clean_data():
+    data = pd.read_csv("../data/data.csv")
+    data = data.drop(["Unnamed: 32", "id"], axis=1)
+    data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0})
+    
+    return data
+
+def main():
+    data = get_clean_data()
 
 if __name__ == "__main__":
     main()
