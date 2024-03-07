f6c94e82ce0de92b32e6f0d1816beffbb96fd179
Darryl Lardizabal <dsteven12@gmail.com>
2024-03-06 18:32:20 -0800

    Change class name to be malignant as well.

--- app/prediction.py
+++ app/prediction.py
@@ -47,5 +47,5 @@ def add_predictions(input_data):
         st.write("<span class='diagnosis malignant'>Malignant</span>", unsafe_allow_html=True)
 
     st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
-    st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
+    st.write("Probability of being malignant: ", model.predict_proba(input_array_scaled)[0][1])
     st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
