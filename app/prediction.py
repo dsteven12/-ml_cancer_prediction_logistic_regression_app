313378df88adbad7d3743611dd2e562aa1d289e3
Darryl Lardizabal <dsteven12@gmail.com>
2024-03-06 18:27:54 -0800

    Changed word choice for prediction

--- app/prediction.py
+++ app/prediction.py
@@ -44,7 +44,7 @@ def add_predictions(input_data):
     if prediction[0] == 0:
         st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
     else:
-        st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
+        st.write("<span class='diagnosis malignant'>Malignant</span>", unsafe_allow_html=True)
 
     st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
     st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
